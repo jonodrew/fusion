c68d42b8aaa9c8f0238637ca7c6663f2c6342b30
Jonathan Kerr <jonathandrewkerr@gmail.com>
2018-02-01 21:28:47 +0000

    Updated tests to use less code

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import random
 import sys
+
+import itertools
 import munkres
 import numpy as np
 import os
@@ -6,10 +9,10 @@ import time
 from operator import itemgetter
 from typing import List, Dict
 import sendgrid
-import yaml
-from matching.classes import Match
+from config import Config
+from matching.classes import Match, Post, FastStreamer
 from sendgrid.helpers.mail import *
-from tests.conftest import test_data
+from tests.conftest import departments, skills, locations, anchors, clearances, test_data
 
 
 def send_email(to_name: str, from_email: str, to_email: str, subject: str, content: str,
@@ -73,23 +76,44 @@ def calculate_matches(list_of_matches: List[Match]):
     return {'aggregate': aggregate, 'processing': total_time, 'matches': final_matches}
 
 
+def demo_mode():
+    data = test_data()
+    l_p = data[2]
+    l_fs = data[1]
+    for p in l_p:
+        print("Post {}:\n\tSkills: {}, {}\n\tAnchor: {}\n\tClearance: {}\n\tLocation: {}\n\tDepartment: {}\n\t "
+              "Private Office: {}".format(p.identifier, p.skills[0], p.skills[1], p.anchor, p.clearance, p.location,
+                                          p.department, p.is_private_office))
+
+    for f in l_fs:
+        print("Candidate {}:\n\tSkills: {}, {}\n\tAnchors: {}, {}\n\tClearance\n\tSkills: {}\n\tDepartments: {}\n\t"
+              "Will undertake DV: {}\n\tnWould like Private Office {}\n".
+              format(f.identifier, f.preferences.skills[0], f.preferences.skills[1], f.preferences.anchors[1],
+                     f.preferences.anchors[2], f.clearance, ', '.join(f.preferences.skills),
+                     ', '.join(f.preferences.departments), f.preferences.will_undertake_dv,
+                     f.preferences.wants_private_office))
+    number = itertools.count().__next__
+    return [Match(identifier=number(), fser_object=f, post_object=p) for f in l_fs for p in l_p]
+
+
 def main():
     env = os.getenv("ENVIRONMENT")
-    l_m = test_data()  # will contain data drawn from database
+    # l_m = test_data()  # will contain data drawn from database
     # for m in l_m:
     #     m.total = randrange(1, 101)
+    l_m = demo_mode()
     results = calculate_matches(l_m)
     print("Run {}: \n\tAverage score: {}\n\tProcessing time: {}".format(len(results['matches']), results['aggregate'] /
                                                                         len(results['matches']), results['processing']))
     for m in results['matches']:
-        if m.total < 15:
+        if m.total < 18:
             """Example output - at scale you wouldn't read it all but you could group and send to CLs"""
             ws = m.weighted_scores
             print("\nFastStreamer id: {}\nPost id: {}\nMatch score: {}\n\tAnchor score: {}\n\tLocation score: {}\n\t"
                   "Skills score: {}\n\n".format(m.fast_streamer.identifier, m.post.identifier, m.total,
                                                 ws['anchor'], ws['location'], ws['skills']))
     match_scores = [m.total for m in results['matches']]
-    histo = np.histogram(match_scores, bins=[x for x in range(0, max(match_scores)+1)])
+    histo = np.histogram(match_scores, bins=[x for x in range(0, max(match_scores) + 1)])
     print(histo[0], "\n", histo[1])
     if env == "test":
         send_email(to_name="Subject X", from_email="test@example.com", to_email="jonathandrewkerr@gmail.com",
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 from random import randrange, choice
-from typing import List
+from typing import List, Tuple
 import random
 import os
 import itertools
@@ -8,7 +8,8 @@ import pytest
 from matching.classes import FastStreamer, Post, Match
 
 departments = ['HO', 'DWP', 'HMRC', 'DH', 'CO', 'GDS', 'MOD', 'DDCMS', 'DCLG', 'DEFRA', 'MOJ', 'DFT', 'DFE', 'DFID']
-skills = ['Project and People Management', 'Change Management']
+skills = ['Software Engineering', 'User Research', 'Strategy & Policy', 'Product Design', 'Content & Analysis',
+          'Delivery', 'Operations', 'Commercial Management']
 anchors = ['Digital', 'Corporate', 'Operations']
 locations = ['London', 'South West', 'Midlands', 'Scotland', 'The North', 'Overseas']
 clearances = ['BPSS', 'CTC', 'SC', 'DV']
@@ -58,21 +59,22 @@ def model_fser_with_prefs() -> FastStreamer:
 
 
 @pytest.fixture
-def test_data() -> List[Match]:
+def test_data() -> Tuple[List[Match], List[FastStreamer], List[Post]]:
     if os.getenv("ENVIRONMENT") == "test":
         amount = 1500
     else:
         amount = 100
     l_p = [Post(skills=random.sample(skills, 2), identifier=i,
-                anchor=random_select(anchors), clearance=random_select(clearances), location=random_select(locations),
-                department=random_select(departments)) for i in range(amount)]
-
+                anchor=random.choice(anchors), clearance=random.choice(clearances),
+                location=random.choice(locations),
+                department=random.choice(departments), private_office=not bool(random.choice(range(0, 10)))) for i in
+           range(amount)]
     l_fs = [FastStreamer(identifier=i, clearance='SC') for i in range(amount, 2 * amount)]
     for f in l_fs:
-        f.set_preferences(skills=random.sample(skills, 2), anchors={1: random_select(anchors),
-                                                                    2: random_select(anchors)},
+        f.set_preferences(skills=random.sample(skills, 2), anchors={1: random.choice(anchors),
+                                                                    2: random.choice(anchors)},
                           departments=random.sample(departments, 10),
                           loc=random.sample(locations, 2), sec=random.choice([True, False]),
                           dv=random.choice([True, False]), po=random.choice([True, False]))
     number = itertools.count().__next__
-    return [Match(identifier=number(), fser_object=f, post_object=p) for f in l_fs for p in l_p]
+    return [Match(identifier=number(), fser_object=f, post_object=p) for f in l_fs for p in l_p], l_fs, l_p
