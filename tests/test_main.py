086861f997b325d75709b10f96569a680c88d4d1
Jonathan Kerr <jonathandrewkerr@gmail.com>
2018-02-02 10:53:40 +0000

    Adjusted test

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,6 @@ from main import calculate_matches
 
 
 def test_calculate_matches(test_data):
-    output = calculate_matches(test_data)
+    output = calculate_matches(test_data[0])
     assert type(output) == dict
     assert len(output['matches'])*10 <= output['aggregate']
