ddae8f5eebbddb299e01864fbd30dfeeef28299b
Jonathan Kerr <jonathandrewkerr@gmail.com>
2018-02-08 11:23:06 +0000

    Changed to local postgres for development

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath((os.path.dirname(__name__)))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "fivefourthreetoone"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "postgresql:///test_local"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
